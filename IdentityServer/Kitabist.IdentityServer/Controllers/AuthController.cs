982458f0d9268e5a0addf0aaabd139b70ce5f47a
Vusal Aliyev <vusalaliev02@gmail.com>
2024-04-17 00:42:37 +0400

    building jwt

---
+++ IdentityServer/Kitabist.IdentityServer/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Kitabist.IdentityServer.DTOs;
+using Kitabist.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Kitabist.IdentityServer.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            var userCredentials = new ApplicationUser()
+            {
+                Email=userRegisterDto.Email,
+                UserName=userRegisterDto.Username,
+                Name=userRegisterDto.Name,
+                Surname=userRegisterDto.Surname
+            };
+            var result = await _userManager.CreateAsync(userCredentials, userRegisterDto.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok("User registered successfully");
+            }
+            else
+            {
+                return Ok("Problem occured");
+            }
+        }
+        //[HttpPost]
+        //public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        //{
+
+        //}
+    }
+}
---
+++ IdentityServer/Kitabist.IdentityServer/Migrations/20240416163038_mig2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kitabist.IdentityServer.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
